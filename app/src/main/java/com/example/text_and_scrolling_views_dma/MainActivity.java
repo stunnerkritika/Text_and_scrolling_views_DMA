e0799d94021361186c32d17ec84b90e979d42839
stunnerkritika <thapakritika019@gmail.com>
2021-03-09 18:58:09 +0545

    Initial Commit

---
+++ app/src/main/java/com/example/text_and_scrolling_views_dma/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.text_and_scrolling_views_dma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
